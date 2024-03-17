d913847585b9fb1b6edd17634c42a45f32e47e2c
Teodor469 <lifetroll6@gmail.com>
2024-03-17 15:15:12 +0200

    .

--- login.php
+++ login.php
@@ -17,9 +17,9 @@
         $errors = [];
 
         if(empty($username)) {
-            $errors[] = "You must enter a username!";
+            $errors['username'] = "You must enter a username!";
         } elseif(empty($password)) {
-            $errors[] = "Can't login without a password!";
+            $errors['password'] = "Can't login without a password!";
         }
 
         if(!empty($username) && !empty($password)) {
@@ -29,17 +29,11 @@
             if ($username_validation && $password_validation) {
                 header("Location: welcome.php");
             } else {
-                $errors[] = 'Username or password is incorrect';
+                $errors['not_match'] = 'Username or password is incorrect';
             }
         }
 
 }
-
-    if (!empty($errors)) {
-        foreach ($errors as $error) {
-          echo $error . "<br>";
-        }
-      }
 
     ?>
 
@@ -47,13 +41,23 @@
     <form action="login.php", method="POST">
         <label for="username">Username: </label>
         <input type="text" name="username">
+        <?php if (isset($errors['username'])) {
+        echo '<span style="color: red;">' . $errors['username'] . '</span>';
+        } ?>
 
         <br>
 
         <label for="password">Password: </label>
         <input type="password" name="password">
+        <?php if (isset($errors['password'])) {
+        echo '<span style="color: red;">' . $errors['password'] . '</span>';
+        } ?>
 
         <br>
+
+        <?php if (isset($errors['not_match'])) {
+        echo '<span style="color: red;">' . $errors['not_match'] . '</span>' . '<br>';
+        } ?>
 
         <input type="submit" name="login">
     </form>
