6a40d337fe5f11e3d84b5652522deb732725f88b
Teodor469 <lifetroll6@gmail.com>
2024-04-10 13:44:41 +0300

    .

--- views/home.view.php
+++ views/home.view.php
@@ -4,8 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <!-- <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet"> -->
-    <link href="/login-page/public/css/style.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
 </head>
 <body>
     <h1 class="text-3xl text-blue-600">Hello Tailwind</h1>
